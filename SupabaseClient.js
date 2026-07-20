416e1a1070e8f89403f99a4fce33ebd5b2b62c07
sirajulummahacademy763-ui <sirajulummahacademy763@gmail.com>
2026-07-20 14:07:02 +0100

    Update SupabaseClient.js

--- SupabaseClient.js
+++ SupabaseClient.js
@@ -1,6 +1,6 @@
 
 const supabase = window.supabase.createClient(
- https://ibpnjsixzutmolzvfolv.supabase.co ,
+ "https://ibpnjsixzutmolzvfolv.supabase.co" ,
  
-  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlicG5qc2l4enV0bW9senZmb2x2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1MzU4ODUsImV4cCI6MjEwMDE
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlicG5qc2l4enV0bW9senZmb2x2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1MzU4ODUsImV4cCI6MjEwMDE"
 );
